b376a4e65bcf483a99dda7cee167bfaf82e7b91e
CHENG VARYROITANAK <varyroitanak@gmail.com>
2026-04-06 21:51:30 +0700

    inital commit

---
+++ slotmachine.py
@@ -0,0 +1,61 @@
+MAX_LINES = 3
+MAX_BET = 670
+MIN_BET = 1
+
+
+def deposit():
+    while True: 
+        amount = input("How much do you wanna gamble broski? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0: 
+                break
+            else: 
+                print("It must be positive u dumbass.")
+        else: 
+            print("Enter a positive number please vro.")
+
+    return amount
+def get_bet():
+    while True:
+        amount = input("How much are you betting, fellow gambler? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else: 
+                print (f"The number must be between ${MIN_BET} - ${MAX_BET}.")
+        else: 
+            print("Enter a positive number please vro.")
+                
+    return amount
+
+def get_number_of_lines():
+    while True: 
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES: 
+                break
+            else: 
+                print("Enter a valid number of lines.")
+        else: 
+            print("Enter a line.")
+    return lines
+
+def main():
+
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        
+        if total_bet > balance:
+            print(f"you do not have enough to bet that amount, your current balance is {balance}.")
+        else: 
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    print(balance, lines)
+main()
